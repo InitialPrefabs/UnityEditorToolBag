95a3f751113c59cce06f192c5ac1868e105e1602
Porrith Suong <psuong95@gmail.com>
2018-12-19 09:20:43 -0500

    Templates out message toggling attribute.

---
+++ Editor/Attributes/Messages/MsgToggleAttributePropertyDrawer.cs
@@ -0,0 +1,13 @@
+﻿using InitialPrefabs.Attributes.Messages;
+using UnityEngine;
+using UnityEditor;
+
+namespace InitialPrefabs.Editor.Attributes.Messages {
+
+    [CustomPropertyDrawer(typeof(MsgToggleAttribute))]
+    public class MsgToggleAttributePropertyDrawer : BasePropertyDrawer {
+
+        protected override void OnInspectorAttribute(Rect rect, SerializedProperty prop, GUIContent label) =>
+            throw new System.NotImplementedException();
+    }
+}
---
+++ Scripts/Attributes/Messages/MsgToggleAttribute.cs
@@ -0,0 +1,40 @@
+﻿namespace InitialPrefabs.Attributes.Messages {
+
+    /// <summary>
+    /// Stores a message to display for a boolean field.
+    /// </summary>
+    public class MsgToggleAttribute : MsgAttribute {
+
+        public bool isInverted;
+
+        public MsgToggleAttribute(string message, MessageLevel messageLevel) : base(message) {
+            this.messageLevel = (int)messageLevel;
+        }
+
+        public MsgToggleAttribute(string message, int height) : base(message) {
+            this.height = height;
+        }
+
+        public MsgToggleAttribute(string message, int height, MessageLevel messageLevel) : base(message, height) {
+            this.messageLevel = (int)messageLevel;
+        }
+
+        public MsgToggleAttribute(string message, bool isInverted) : base(message) {
+            this.isInverted = isInverted;
+        }
+
+        public MsgToggleAttribute(string message, bool isInverted, MessageLevel messageLevel) : 
+            this(message, isInverted) {
+            this.messageLevel = (int)messageLevel;
+        }
+
+        public MsgToggleAttribute(string message, int height, bool isInverted) : base(message, height) {
+            this.isInverted = isInverted;
+        }
+
+        public MsgToggleAttribute(string message, int height, bool isInverted, MessageLevel messageLevel) : 
+            this(message, height, isInverted) {
+            this.messageLevel = (int)messageLevel;
+        }
+    }
+}
