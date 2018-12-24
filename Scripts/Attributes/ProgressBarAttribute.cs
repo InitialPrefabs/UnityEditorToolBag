b63eeb720b8564fe7115092c7f8ef4966e4aec41
Porrith Suong <psuong95@gmail.com>
2018-12-24 01:14:44 -0500

    Adds a progressbar example.

---
+++ Scripts/Attributes/ProgressBarAttribute.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace InitialPrefabs.Attributes {
+
+    /// <summary>
+    /// Stores a max value for the progress bar.
+    /// </summary>
+    public class ProgressBarAttribute : PropertyAttribute {
+        
+        public float max;
+        public string label;
+
+        public ProgressBarAttribute() {
+            max   = 100f;
+        }
+
+        public ProgressBarAttribute(string label) : this() {
+            this.label = label;
+        }
+
+        public ProgressBarAttribute(float max) : this() { 
+            this.max = max;
+        }
+
+        public ProgressBarAttribute(float max, string label) : this(max) {
+            this.label = label;
+        }
+    }
+}
---
+++ Scripts/Examples/ProgressBarExample.cs
@@ -0,0 +1,17 @@
+﻿using InitialPrefabs.Attributes;
+using UnityEngine;
+
+namespace InitialPrefabs.Examples {
+
+    /// <summary>
+    /// Shows examples of progress bars.
+    /// </summary>
+    public class ProgressBarExample : MonoBehaviour {
+        
+        [ProgressBar("Int")]
+        public int intValue;
+
+        [ProgressBar(25, "Float")]
+        public float floatValue;
+    }
+}
